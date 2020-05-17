7053f23419f0edbf8b960abdb50fa59b4f4d9c33
stadu <stadu@umich.edu>
2020-05-17 18:19:37 -0400

    Updates

---
+++ precache-manifest.58a54824c0187b66091ced045163c964.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faaf31d18769c003ef78450780bd2e5b",
+    "url": "/maze/index.html"
+  },
+  {
+    "revision": "70c31d5a02cff4ebcfd5",
+    "url": "/maze/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "e555f71e170aec247dce",
+    "url": "/maze/static/css/main.863b417d.chunk.css"
+  },
+  {
+    "revision": "70c31d5a02cff4ebcfd5",
+    "url": "/maze/static/js/2.f43e4fce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maze/static/js/2.f43e4fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e555f71e170aec247dce",
+    "url": "/maze/static/js/main.3317c7b6.chunk.js"
+  },
+  {
+    "revision": "372ddbc0db47d9b9c1b7",
+    "url": "/maze/static/js/runtime-main.d8626bd7.js"
+  }
+]);
